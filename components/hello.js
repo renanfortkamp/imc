323719ae2b9b50b53c9d7badd668fd1d3e91baf6
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2022-08-10 17:24:45 -0300

    IMC

---
+++ components/hello.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import { StyleSheet, Text, View, Input } from 'react-native';
+
+function hello(){
+  return <Text>Hello World</Text>
+    
+  
+  
+}
+
+export default hello;
